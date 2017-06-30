95363d9d7683f3729918345c7e0e3a9ad49afe93
rahulmiddha <rahulmiddha@gmail.com>
2017-06-30 15:38:20 -0400

    Add files via upload

---
+++ js/receiverApplication.js
@@ -0,0 +1,122 @@
+var flashTimer;
+
+function flash(msg) {
+    window.clearTimeout(flashTimer);
+    var f = document.getElementById('flash');
+    f.firstChild.innerHTML = msg;
+    f.style.opacity = 1;
+    flashTimer = window.setTimeout(function() {
+        f.style.opacity = 0;
+    },2000);
+}
+
+var Shim = (function() {
+    function Shim(url,config) {
+        this.config = config||{};
+        this.initialize();
+    }
+    Shim.prototype.initialize = function() {
+        this.data = {
+            scale: 1,
+            aspect: 'native',
+            rotation: 0,
+            overscan: [0,0,0,0],
+            displayId: window.location.href.split('/').pop()
+        };
+        this.displayId = window.location.href.split('/').pop();
+        this.frame = document.createElement('iframe');
+        document.body.appendChild(this.frame);
+        var self = this;
+        window.onresize = function() {
+            self.reflow();
+        };
+        this.reflow();
+    };
+    Shim.prototype.update = function(data) {
+        if (data.url && data.url !== this.data.url) {
+            this.seturl(data.url);
+        }
+        this.data = data;
+        this.reflow();
+    };
+    Shim.prototype.seturl = function(url) {
+        this.frame.src = url;
+    };
+    Shim.prototype.getScale = function() {
+        return this.data.zoom||1;
+    };
+    Shim.prototype.getViewport = function() {
+        var w = document.body.clientWidth-this.data.overscan[1]-this.data.overscan[3];
+        var h = document.body.clientHeight-this.data.overscan[0]-this.data.overscan[2];
+        return {
+            w: w,
+            h: h,
+            a: w/h
+        };
+    };
+    Shim.prototype.getAspect = function() {
+        if (typeof this.data.aspect==='number') {
+            return this.data.aspect;
+        } else {
+            return this.getViewport().a;
+        }
+    };
+    Shim.prototype.reflow = function() {
+        var os = this.data.overscan;
+        //pixel size of the display
+        var cw = document.body.clientWidth-os[1]-os[3];
+        var ch = document.body.clientHeight-os[0]-os[2];
+        var vp = this.getViewport();
+        var scale = this.getScale();
+        this.scaleX = vp.a*scale/this.getAspect();
+        this.scaleY = scale;
+        //pixel size of the viewport (scaled)
+        var bw = vp.w/this.scaleX;
+        var bh = vp.h/this.scaleY;
+        //size of the rotated viewport
+        var width = ((this.data.rotation%180)===90)?bh:bw;
+        var height = ((this.data.rotation%180)===90)?bw:bh;
+
+        var style = '';
+        var rotate = 'rotate('+(this.data.rotation||0)+'deg)';
+        var scaleX = 'scaleX('+this.scaleX+')';
+        var scaleY = 'scaleY('+this.scaleY+')';
+        var transform = scaleX+' '+scaleY+' '+rotate+';';
+        style += '-webkit-transform: '+transform;
+        style += '-moz-transform: '+transform;
+        style += 'transform: '+transform;
+        style += 'width:'+width+'px;';
+        style += 'height:'+height+'px;';
+        //center
+        style += 'margin-left:'+(-width/2)+'px;';
+        style += 'margin-top:'+(-height/2)+'px;';
+        this.frame.setAttribute('style',style);
+        //body style to adjust for overscan
+        document.body.style.left = ((os[3]-os[1])/2)+'px';
+        document.body.style.top = ((os[0]-os[2])/2)+'px';
+    };
+
+    return Shim;
+}());
+
+var shim = new Shim();
+
+function setupChromcast() {
+    var castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
+    var customMessageBus = castReceiverManager.getCastMessageBus(
+        'urn:x-cast:org.firstlegoleague.castDeck',
+        cast.receiver.CastMessageBus.MessageType.JSON
+    );
+    customMessageBus.onMessage = function(event) {
+        var str = JSON.stringify(event.data);
+        shim.update(event.data);
+        // send something back
+        customMessageBus.send(event.senderId,{
+            requestId: event.data.requestId,
+            data: 'ack'
+        });
+    };
+    castReceiverManager.start();
+}
+
+setupChromcast();
---
+++ js/senderApplication.js
@@ -0,0 +1,150 @@
+var session;
+var zoom = 1;
+var rotation = 0;
+var url;
+var overscan = [0,0,0,0];
+var aspect = 'native';
+
+function cast(cb) {
+    console.log('cast');
+    chrome.cast.requestSession(function(_session) {
+        console.log('has session',arguments);
+
+        session = _session;
+
+        session.sendMessage(
+            'urn:x-cast:org.firstlegoleague.castDeck',
+            'hello world',
+            function() {
+                console.log('msg sent');
+            },
+            function() {
+                console.log('msg err');
+            }
+        );
+        session.addMessageListener(
+            'urn:x-cast:org.firstlegoleague.castDeck',
+            function() {
+                console.log('got msg',arguments);
+            }
+        );
+        if (cb) {
+            cb();
+        }
+    }, function() {
+        console.log('has error',arguments);
+    });
+}
+
+function sendMessage(obj) {
+    var str = JSON.stringify(obj);
+    session.sendMessage(
+        'urn:x-cast:org.firstlegoleague.castDeck',
+        str,
+        function() {
+            console.log('update sent');
+        },
+        function() {
+            console.log('update err');
+        }
+    );
+}
+
+function sendUpdate() {
+    sendMessage({
+        url: url,
+        rotation: rotation,
+        zoom: zoom,
+        aspect: aspect,
+        overscan: overscan
+    });
+}
+
+function stop() {
+    console.log('stop');
+    session.stop();
+}
+
+function setZoom(z) {
+    zoom = z;
+    document.getElementById('zoomLevel').innerHTML = zoom.toFixed(1);
+    sendUpdate();
+}
+
+function zoomIn() {
+    setZoom(zoom + 0.1);
+}
+
+function zoomOut() {
+    setZoom(zoom - 0.1);
+}
+
+function zoomReset() {
+    setZoom(1);
+}
+
+function rotateCCW() {
+    rotation = (rotation+270) % 360;
+    sendUpdate();
+}
+
+function rotateCW() {
+    rotation = (rotation+90) % 360;
+    sendUpdate();
+}
+
+function updateUrl() {
+    url = document.getElementById('url').value;
+    sendUpdate();
+}
+
+function adjustTop(value) {
+    overscan[0] = value;
+    sendUpdate();
+}
+function adjustRight(value) {
+    overscan[1] = value;
+    sendUpdate();
+}
+function adjustBottom(value) {
+    overscan[2] = value;
+    sendUpdate();
+}
+function adjustLeft(value) {
+    overscan[3] = value;
+    sendUpdate();
+}
+
+function updateAspect(value) {
+    aspect = value;
+    sendUpdate();
+}
+
+function initCast() {
+    console.log('cast api');
+    var sessionRequest = new chrome.cast.SessionRequest('4EC978AD');
+    var apiConfig = new chrome.cast.ApiConfig(sessionRequest,
+        function(_session) {
+            console.log('existing session',arguments);
+            session = _session;
+        },
+        function() {
+            console.log('receiverListener',arguments);
+        }
+    );
+    chrome.cast.initialize(apiConfig,
+        function() {
+            console.log('init success');
+        }, function() {
+            console.log('init error');
+        }
+    );
+}
+
+window['__onGCastApiAvailable'] = function(loaded, errorInfo) {
+    if (loaded) {
+        initCast();
+    } else {
+        console.log(errorInfo);
+    }
+};
